4e9c421e886cab84e2030977768b90516b30a9bb
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-10-10 19:44:03 +0300

    DbContext and Configuration and Database

--- ECommerce/Program.cs
+++ ECommerce/Program.cs
@@ -1,4 +1,7 @@
 
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data.Contexts;
+
 namespace ECommerce
 {
     public class Program
@@ -13,6 +16,10 @@ namespace ECommerce
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddDbContext<StoreDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
---
+++ Infrastructure/Persistence/Data/Contexts/StoreDbContext.cs
@@ -0,0 +1,19 @@
+﻿using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence.Data.Contexts
+{
+    public class StoreDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductBrand> ProductBrands { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public StoreDbContext(DbContextOptions<StoreDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(StoreDbContext).Assembly);
+        }
+    }
+}
