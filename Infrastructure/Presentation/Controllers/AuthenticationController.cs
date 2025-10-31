338c4d011389553d4f4e7f67b07e4f03cf041b4c
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-10-31 15:12:55 +0200

    Authentication controller

---
+++ Infrastructure/Presentation/Controllers/AuthenticationController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceAbstraction;
+using Shared.IdentityDto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthenticationController(IServiceManager _serviceManager) : ControllerBase
+    {
+
+
+        [HttpPost("Login")]
+        public ActionResult<UserDto> Login( LoginDto request)
+        {
+             var user=_serviceManager.AuthenticationService.LoginAsync(request);
+                return Ok(user);
+        }
+
+        [HttpPost("Register")]
+        public ActionResult<UserDto> Register(RegisterDto request)
+        {
+            var user = _serviceManager.AuthenticationService.RegisterAsync(request);
+            return Ok(user);
+        }
+    }
+}
