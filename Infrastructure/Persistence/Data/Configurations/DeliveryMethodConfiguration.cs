afec2af6dd8def0a45fc72b7fac297e910fcdee3
Mahmoud Amr <112844759+Mahmoud-Amr11@users.noreply.github.com>
2025-11-22 13:03:33 +0200

    Add order configuration

---
+++ Infrastructure/Persistence/Data/Configurations/DeliveryMethodConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using DomainLayer.Models.OrderModules;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Data.Configurations
+{
+    public partial class DeliveryMethodConfiguration : IEntityTypeConfiguration<DeliveryMethod>
+    {
+        public void Configure(EntityTypeBuilder<DeliveryMethod> builder)
+        {
+            builder.Property(d => d.Price)
+                .HasColumnType("decimal(8,2)");
+
+            builder.Property(d => d.ShortName)
+                .HasColumnType("varchar(50)");
+
+            builder.Property(d => d.Description)
+              .HasColumnType("varchar(100)");
+
+            builder.Property(d => d.DeliveryTime)
+                .HasColumnType("varchar(50)");
+
+        }
+    }
+}
---
+++ Infrastructure/Persistence/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using DomainLayer.Models.OrderModules;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistence.Data.Configurations
+{
+    public partial class DeliveryMethodConfiguration
+    {
+        public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+        {
+            public void Configure(EntityTypeBuilder<OrderItem> builder)
+            {
+                builder.Property(oi => oi.Price)
+                    .HasColumnType("decimal(8,2)");
+
+                builder.OwnsOne(oi => oi.Product);
+            }
+        }
+    }
+}
